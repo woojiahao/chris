99579a95c60a5c31890332d2ff538789bce68046
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-21 00:06:04 +0800

    Add readNumber and substring fn for Expression

--- internal/lexer/expression.go
+++ internal/lexer/expression.go
@@ -1,5 +1,10 @@
 package lexer
 
+import (
+	"strconv"
+	"unicode"
+)
+
 type Expression string
 
 func (exp *Expression) Get(index int) *rune {
@@ -8,6 +13,48 @@ func (exp *Expression) Get(index int) *rune {
 	}
 	c := []rune(*exp)[index]
 	return &c
+}
+
+// substring retrieves the substring from start to end (both inclusive)
+func (exp *Expression) substring(start, end int) *string {
+	if end >= len(*exp) {
+		end = len(*exp) - 1
+	}
+
+	if start < 0 {
+		start = 0
+	}
+
+	substring := (*exp)[start : end+1]
+	casted := string(substring)
+
+	return &casted
+}
+
+func (exp *Expression) readNumber(start int) (*float64, int) {
+	i := start
+	for {
+		cur := exp.Get(start)
+
+		if cur == nil {
+			return nil, -1
+		}
+
+		if unicode.IsDigit(*cur) {
+			i++
+		} else {
+			break
+		}
+	}
+
+	// TODO: Add unit test for this
+	numStr := *exp.substring(start, i)
+	num, err := strconv.ParseFloat(numStr, 64)
+	if err != nil {
+		return nil, -1
+	}
+
+	return &num, i + 1
 }
 
 func (exp *Expression) Len() int {
