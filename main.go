a25d90f5979f7059aaede8f83f8804a45d2e1766
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-21 00:29:03 +0800

    Add use of Lexer into main

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"woojiahao.com/chris/internal/lexer"
+)
 
 func main() {
-	fmt.Print("Hello world!\n")
+	l := lexer.New("12 34")
+	fmt.Printf("lexer.Next step 1 returns %v", l.Next())
+	fmt.Printf("lexer.Next step 2 returns %v", l.Next())
 }
