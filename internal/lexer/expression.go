da350b30efb27acac535fde14dbdd2e5089f32b5
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-21 00:26:56 +0800

    Fix Expression#readNumber not parsing till end
    When reading a number which happens to be the last value, the original
    implementation would fail to parse the next character and error.
    However, by adding a check if the next element is the last value and cur
    is nil already, we can tell that we have reached the end of the
    expression and as such, can just parse whatever we got.
    
    TODO to improve and streamline this logic

--- internal/lexer/expression.go
+++ internal/lexer/expression.go
@@ -8,7 +8,7 @@ import (
 type Expression string
 
 func (exp *Expression) Get(index int) *rune {
-	if index > len(*exp) {
+	if index >= exp.Len() {
 		return nil
 	}
 	c := []rune(*exp)[index]
@@ -17,8 +17,8 @@ func (exp *Expression) Get(index int) *rune {
 
 // substring retrieves the substring from start to end (both inclusive)
 func (exp *Expression) substring(start, end int) *string {
-	if end >= len(*exp) {
-		end = len(*exp) - 1
+	if end >= exp.Len() {
+		end = exp.Len() - 1
 	}
 
 	if start < 0 {
@@ -34,10 +34,13 @@ func (exp *Expression) substring(start, end int) *string {
 func (exp *Expression) readNumber(start int) (*float64, int) {
 	i := start
 	for {
-		cur := exp.Get(start)
+		cur := exp.Get(i)
 
-		if cur == nil {
+		if cur == nil && i < exp.Len() {
+			// If the cur is nil and we're not at the end yet, return
 			return nil, -1
+		} else if cur == nil && i >= exp.Len() {
+			break
 		}
 
 		if unicode.IsDigit(*cur) {
@@ -48,13 +51,13 @@ func (exp *Expression) readNumber(start int) (*float64, int) {
 	}
 
 	// TODO: Add unit test for this
-	numStr := *exp.substring(start, i)
+	numStr := *exp.substring(start, i-1)
 	num, err := strconv.ParseFloat(numStr, 64)
 	if err != nil {
 		return nil, -1
 	}
 
-	return &num, i + 1
+	return &num, i
 }
 
 func (exp *Expression) Len() int {
