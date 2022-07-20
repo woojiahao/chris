8b733722ab5672578986c79e5dfc80c3fd9ddd4f
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-20 23:28:11 +0800

    Add Expression struct
    Include built-in functions to handle retrieving character at index

---
+++ internal/lexer/expression.go
@@ -0,0 +1,15 @@
+package lexer
+
+import (
+	"strconv"
+)
+
+type Expression string
+
+func (exp *Expression) Get(index int) *string {
+	if index > len(*exp) {
+		return nil
+	}
+	c := strconv.Itoa(int((*exp)[index]))
+	return &c
+}
