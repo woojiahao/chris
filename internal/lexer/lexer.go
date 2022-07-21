89d2e4a485f804b07425d739230314d05be9500d
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-21 10:36:25 +0800

    Add Peek function for Lexer
    Peek will return the next index instead of actually moving the index in the
    Lexer. This way, we can develop Next() to actually consume

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -14,6 +14,37 @@ func New(exp Expression) *Lexer {
 	return &Lexer{&exp, 0, nil}
 }
 
+// Peek reads the next non-whitespace token and returns the token and the index after reading the token
+func (l *Lexer) Peek() (*Token, int) {
+	i := l.index
+	for i < l.expression.Len() {
+		cur := l.expression.Get(i)
+
+		if cur == nil {
+			// Unable to retrieve character at given index
+			return nil, -1
+		}
+
+		if unicode.IsSpace(*cur) {
+			// Move pointer forward if cur is whitespace since we aren't reading whitespaces
+			i++
+			continue
+		}
+
+		if unicode.IsDigit(*cur) {
+			// Read the next few characters for the number
+			num, next := l.expression.readNumber(i)
+			l.token = &Token{Number, *num, *cur}
+			return l.token, next
+		}
+
+		l.token = &Token{Variable, 0, '0'}
+		return l.token, i + 1
+	}
+
+	return nil, -1
+}
+
 // Next returns token at the current position of index and moves the head to the next character for further calls
 func (l *Lexer) Next() *Token {
 	if l.index >= l.expression.Len() {
