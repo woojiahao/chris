9df8add1afeaa7db2e70f70641e4440d002e66a2
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-23 23:17:04 +0800

    Create Any iteration fn
    Function used to check if any element in a given list matches the given predicate

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -22,6 +22,15 @@ func FuzzyMatch(target string, input string) bool {
 	return true
 }
 
+func Any[V comparable](lst []V, predicate func(V) bool) bool {
+	for _, v := range lst {
+		if predicate(v) {
+			return true
+		}
+	}
+
+	return false
+}
 func StrToFloat(str string) *float64 {
 	num, err := strconv.ParseFloat(str, 64)
 	if err != nil {
