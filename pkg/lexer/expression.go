c55fce8de698572074a28012476ae30cf7bc797b
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-23 22:14:56 +0800

    Add lookAhead logic to Expression
    Allow an expression to check the immediate token at or after a given index

--- pkg/lexer/expression.go
+++ pkg/lexer/expression.go
@@ -88,3 +88,63 @@ func (exp Expression) read(i int, predicate func(rune) bool) int {
 
 	return i
 }
+
+// lookAhead checks for the immediate token at or after the given start index in an expression
+func (exp Expression) lookAhead(start int) (*Token, int) {
+	i := start
+	token := &Token{EndOfExpression, 0, ""}
+
+	for i < exp.Len() {
+		cur := exp.Get(i)
+
+		if cur == nil {
+			// Unable to retrieve character at given index
+			return nil, -1
+		}
+
+		if unicode.IsSpace(*cur) {
+			// Move pointer forward if cur is whitespace since we aren't reading whitespaces
+			i++
+			continue
+		}
+
+		if unicode.IsDigit(*cur) {
+			// Read the next few characters for the number
+			num, next := exp.readNumber(i)
+			if num == nil {
+				// If failed to read number, return nil to indicate lexing failed
+				return nil, -1
+			}
+
+			token = NewNumber(*num)
+			i = next
+			break
+		}
+
+		if utils.In(operators, *cur) {
+			token = NewOperator(*cur)
+			i++
+			break
+		}
+
+		if unicode.IsLetter(*cur) {
+			word, next := exp.readWord(i)
+			if word == nil {
+				return nil, -1
+			}
+
+			if len(*word) > 1 {
+				token = NewKeyword(*word)
+			} else {
+				token = NewVariable(*word)
+			}
+			i = next
+			break
+		}
+
+		// If it's not any of our valid variables, return nil to indicate that lexing failed
+		return nil, -1
+	}
+
+	return token, i
+}
