ede5a8f3589dbe702811d70c83365828de01e5ad
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-23 23:16:34 +0800

    Create fuzzy match utils fn
    Fuzzy match checks if a given input matches the start of a target string.
    
    E.g. input "si" would match targets "sin" and "six"

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -12,6 +12,16 @@ func In[V comparable](lst []V, target V) bool {
 	return false
 }
 
+func FuzzyMatch(target string, input string) bool {
+	for i := 0; i < len(input); i++ {
+		if target[i] != input[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
 func StrToFloat(str string) *float64 {
 	num, err := strconv.ParseFloat(str, 64)
 	if err != nil {
