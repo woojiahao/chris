dbf2aa7bfaedc3f9e089b37b13ba5b9e4d4c23cf
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-23 22:16:26 +0800

    Update Lexer use lookAhead for operator-less exps

--- pkg/lexer/lexer.go
+++ pkg/lexer/lexer.go
@@ -2,7 +2,6 @@ package lexer
 
 import (
 	"github.com/woojiahao/chris/internal/utils"
-	"unicode"
 )
 
 // TODO Use regex to check if the current character is an operator? Benchmark this
@@ -13,70 +12,45 @@ type Lexer struct {
 	expression     Expression
 	nextExpression Expression
 	token          *Token
+	keywords       []string
 }
 
-func New(exp string) *Lexer {
-	return &Lexer{Expression(exp), 0, nil}
+func New(exp string, keywords []string) *Lexer {
+	expression := Expression(exp)
+	return &Lexer{
+		expression,
+		expression,
+		nil,
+		keywords,
+	}
 }
 
 // Peek reads the next non-whitespace token and returns the token and the index after reading the token
-func (l *Lexer) Peek() (*Token, int) {
-	i := l.index
-	for i < l.expression.Len() {
-		cur := l.expression.Get(i)
+func (l *Lexer) Peek() (*Token, string) {
+	// Get the very first token in the nextExpression
+	token, j := l.nextExpression.lookAhead(0)
+	// Get the very next token in the nextExpression
+	next, _ := l.nextExpression.lookAhead(j)
 
-		if cur == nil {
-			// Unable to retrieve character at given index
-			return nil, -1
-		}
+	nextExpression := l.nextExpression.substring(j, l.nextExpression.Len()-1)
 
-		if unicode.IsSpace(*cur) {
-			// Move pointer forward if cur is whitespace since we aren't reading whitespaces
-			i++
-			continue
-		}
-
-		if unicode.IsDigit(*cur) {
-			// Read the next few characters for the number
-			num, next := l.expression.readNumber(i)
-			if num == nil {
-				// If failed to read number, return nil to indicate lexing failed
-				return nil, -1
-			}
-
-			l.token = NewNumber(*num)
-			return l.token, next
-		}
-
-		if utils.In(operators, *cur) {
-			l.token = NewOperator(*cur)
-			return l.token, i + 1
-		}
-
-		if unicode.IsLetter(*cur) {
-			word, next := l.expression.readWord(i)
-			if word == nil {
-				return nil, -1
-			}
-
-			if len(*word) > 1 {
-				l.token = NewKeyword(*word)
-			} else {
-				l.token = NewVariable(*word)
-			}
-			return l.token, next
-		}
-
-		// If it's not any of our valid variables, return nil to indicate that lexing failed
-		return nil, -1
+	acceptedPairs := map[TokenType][]TokenType{
+		//Keyword: LeftParenthesis, // pi( (not supported yet until word parsing works properly)
+		Number:   {Variable, Keyword, LeftParenthesis}, // 3x, 3sin, 3(
+		Variable: {Keyword, LeftParenthesis},           // xsin, x(
+		Keyword:  {Variable},                           // pi4
+	}
+	if suffix, ok := acceptedPairs[token.TokenType]; ok && utils.In(suffix, next.TokenType) {
+		nextExpression = "*" + nextExpression
 	}
 
-	return &Token{EndOfExpression, 0, ""}, -1
+	return token, nextExpression
 }
 
 // Next returns token at the current position of index and moves the head to the next character for further calls
 func (l *Lexer) Next() *Token {
 	token, next := l.Peek()
-	l.index = next
+	l.token = token
+	l.nextExpression = Expression(next)
 	return token
 }
