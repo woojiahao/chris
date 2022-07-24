ea687556631ed3a0e4a662c75dae38484667ebf1
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-24 09:31:16 +0800

    Improve error name for function call keyword

--- pkg/parser/parse_error.go
+++ pkg/parser/parse_error.go
@@ -11,7 +11,7 @@ const (
 	invalidEndOfGroup            errorReason = "Expected ) at the end of a group, did not receive it"
 	invalidEndOfFunction                     = "Expected ) at the end of a function call, did not receive it"
 	invalidEndOfAssignment                   = "Expected right expression for assignment, did not receive it"
-	invalidKeywordInFunctionCall             = "Function call must use keyword that has more than 1 character"
+	invalidKeywordInFunctionCall             = "Function call must use assigned keywords"
 	invalidVariableInAssignment              = "Assignment variable must be a single-character value"
 	invalidPrefixToken                       = "Invalid prefix token. Only valid prefix tokens are [<variable><keyword><number>-(]"
 	invalidInfixToken                        = "Invalid infix token. Only valid infix tokens are [+-/*^(=]"
