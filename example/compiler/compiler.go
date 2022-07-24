799bab930103676c94332e3737684db17b472065
Woo Jia Hao <woojiahao1234@gmail.com>
2022-07-24 17:30:59 +0800

    Improve example compiler
    Use operatorFnMap instead of dispatching operator to function using a switch statement

--- example/compiler/compiler.go
+++ example/compiler/compiler.go
@@ -22,19 +22,27 @@ var constantValueMap = map[string]float64{
 	"pi": math.Pi,
 }
 
-type Compiler struct {
+var operatorFnMap = map[string]func(float64, float64) float64{
+	"+": func(a, b float64) float64 { return a + b },
+	"-": func(a, b float64) float64 { return a - b },
+	"*": func(a, b float64) float64 { return a * b },
+	"/": func(a, b float64) float64 { return a / b },
+	"^": func(a, b float64) float64 { return math.Pow(a, b) },
+}
+
+type compiler struct {
 	l *lexer.Lexer
 	p *parser.Parser
 }
 
-func New(exp string) *Compiler {
+func New(exp string) *compiler {
 	l := lexer.New(exp, keywords, constants)
 	p := parser.New(l)
-	return &Compiler{l, p}
+	return &compiler{l, p}
 }
 
 // GenerateFunction generates a function that receives the variable and returns the evaluated body of the equation
-func (c *Compiler) GenerateFunction() func(float64) float64 {
+func (c *compiler) GenerateFunction() func(float64) float64 {
 	ast, err := c.p.Parse()
 	if err != nil {
 		panic(err)
@@ -64,8 +72,7 @@ func recursiveDescent(node parser.Node, variable float64) float64 {
 	case parser.OperatorNode:
 		left := recursiveDescent(n.Left, variable)
 		right := recursiveDescent(n.Right, variable)
-		op := dispatchOperator(n.Operator.Symbol)
-		return op(left, right)
+		return operatorFnMap[n.Operator.Symbol](left, right)
 	case parser.FunctionNode:
 		if len(n.Arguments) != 1 {
 			panic("Supported keywords only require 1 argument")
@@ -81,42 +88,4 @@ func recursiveDescent(node parser.Node, variable float64) float64 {
 	}
 
 	return -1
-}
-
-func dispatchOperator(symbol string) func(float64, float64) float64 {
-	var op func(float64, float64) float64
-	switch symbol {
-	case "+":
-		op = add
-	case "-":
-		op = minus
-	case "/":
-		op = divide
-	case "*":
-		op = multiply
-	case "^":
-		op = exponent
-	}
-
-	return op
-}
-
-func add(a, b float64) float64 {
-	return a + b
-}
-
-func minus(a, b float64) float64 {
-	return a - b
-}
-
-func divide(a, b float64) float64 {
-	return a / b
-}
-
-func multiply(a, b float64) float64 {
-	return a * b
-}
-
-func exponent(a, b float64) float64 {
-	return math.Pow(a, b)
 }
